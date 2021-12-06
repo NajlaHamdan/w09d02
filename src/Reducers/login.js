32352d4033259e91a765eca372d49ba966c10ff6
NajlaHamdan <Najla.Hamdan.elq.js@tuwaiq.edu.sa>
2021-12-06 16:44:24 +0300

    redux

---
+++ src/Reducers/login.js
@@ -0,0 +1,32 @@
+const initialState={
+    user:null,
+    token:""
+}
+
+const signIn=(state=initialState,action)=>{
+    const {type,payload}=action;
+
+    switch(type){
+        case "LOGIN":
+            const {user,token}=payload;
+            return (user,token);
+        case "LOGOUT":
+            return payload;
+        default:
+            return state;
+    }
+}
+export default signIn;
+
+export const login=(data)=>{
+    return{
+        type="LOGIN",
+        payload:data
+    }
+}
+export const logout=(data)=>{
+    return{
+        type="LOGOUT",
+        payload:data
+    }
+}
